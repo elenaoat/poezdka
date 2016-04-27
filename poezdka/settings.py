f679e37eb7a7e83f482e4369bddef86111b9de3f
Elena Oat <elena@rategia.com>
2016-04-27 20:39:19 +0300

    minor changes in settings

--- poezdka/settings.py
+++ poezdka/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!7y+#gg&yd5vw_==#-9z3k)v!q8h^rsg%v)1^(qi9_10b$znpo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['poezhai.com', 'www.poezhai.com', '127.0.0.1', 'localhost']
 
 SITE_ID = 1
 # Application definition
